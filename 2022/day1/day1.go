8bb1e0bc0845e6a3607e45dd54756f404e5143ea
CJ Campbell <cjc@qualtrics.com>
2022-12-01 15:26:45 -0700

    Implement day 1.1

--- 2022/day1/day1.go
+++ 2022/day1/day1.go
@@ -2,6 +2,10 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"math"
+	"strconv"
+	"strings"
 
 	_ "embed"
 )
@@ -10,5 +14,24 @@ import (
 var calorieInventory string
 
 func main() {
-	fmt.Println("calorieInventory:", calorieInventory)
+	lines := strings.Split(calorieInventory, "\n")
+
+	var mostCaloriesSoFar float64
+	var caloriesForCurrentElf float64
+	for _, line := range lines {
+		if line == "" {
+			mostCaloriesSoFar = math.Max(caloriesForCurrentElf, mostCaloriesSoFar)
+			caloriesForCurrentElf = 0
+			continue
+		}
+
+		caloriesInLineItem, err := strconv.Atoi(line)
+		if err != nil {
+			log.Fatal(err)
+		}
+		caloriesForCurrentElf += float64(caloriesInLineItem)
+
+	}
+
+	fmt.Println("The elf with the most calories has", mostCaloriesSoFar)
 }
